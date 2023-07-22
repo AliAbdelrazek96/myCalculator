20e4f8c95fa52b80b87d7b4ad2ecc1e8bf7d56f6
AliAhmed96 <129962715+AliAbdelrazek96@users.noreply.github.com>
2023-07-22 12:09:09 +0300

    add basic functions

---
+++ index.js
@@ -0,0 +1,42 @@
+const ACButton = document.getElementById('AC');
+const plusMinusButton = document.getElementById('plus-minus');
+const percentageButton = document.getElementById('percentage');
+const divideButton = document.getElementById('divide');
+
+const sevenButton = document.getElementById('seven');
+const eightButton = document.getElementById('eight');
+const nineButton = document.getElementById('nine');
+const multiplyButton = document.getElementById('multiply');
+
+const fourButton = document.getElementById('four');
+const fiveButton = document.getElementById('five');
+const sixButton = document.getElementById('six');
+const subtractButton = document.getElementById('subtract');
+
+const oneButton = document.getElementById('one');
+const twoButton = document.getElementById('two');
+const threeButton = document.getElementById('three');
+const addButton = document.getElementById('add');
+
+const zeroButton = document.getElementById('zero');
+const decimalButton = document.getElementById('decimal');
+const equalsButton = document.getElementById('equals');
+
+
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a/b 
+}
+
+
