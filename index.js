9aa9d161c156415aab0a155b381c68b820fa6045
AliAhmed96 <129962715+AliAbdelrazek96@users.noreply.github.com>
2023-07-22 18:25:55 +0300

    remove excess comments/functions

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const ACButton = document.getElementById('AC');
 const plusMinusButton = document.getElementById('plus-minus');
 const percentageButton = document.getElementById('percentage');
 const divideButton = document.getElementById('divide');
-
 const sevenButton = document.getElementById('seven');
 const eightButton = document.getElementById('eight');
 const nineButton = document.getElementById('nine');
 const multiplyButton = document.getElementById('multiply');
-
 const fourButton = document.getElementById('four');
 const fiveButton = document.getElementById('five');
 const sixButton = document.getElementById('six');
 const subtractButton = document.getElementById('subtract');
-
 const oneButton = document.getElementById('one');
 const twoButton = document.getElementById('two');
 const threeButton = document.getElementById('three');
 const addButton = document.getElementById('add');
-
 const zeroButton = document.getElementById('zero');
 const decimalButton = document.getElementById('decimal');
-const equalsButton = document.getElementById('equals');
 
+const equalsButton = document.getElementById('equals');
 const display = document.getElementById('display');
 
 //define variables
@@ -32,17 +28,9 @@ let secondNumber = '';
 let operator = '';
 
 
-// // Function to update the display with the clicked number
-// function updateDisplay(entry) {
-//   display.textContent += entry; //if u remove += the operator 
-//   //will clear out the display!!
-//   currentValue += entry;
-// }
 
 // // Clear the display when AC button is clicked
 ACButton.addEventListener('click', clearThings);
-
-
 function clearThings() {
   display.textContent = ''
   currentValue = ''
@@ -55,7 +43,7 @@ function clearThings() {
 const nums = document.querySelectorAll('.num')
 nums.forEach((button) => {
     button.addEventListener('click', () => {
-      if (operator && !secondNumber) { //toperator = '+', currentValue = '99'
+      if (operator && !secondNumber) { //operator = '+', currentValue = '99'
          display.textContent = '';
          display.textContent += button.textContent; 
          secondNumber += display.textContent //we stop it after secondNumber is true
@@ -71,18 +59,6 @@ nums.forEach((button) => {
         display.textContent += button.textContent;
         currentValue = display.textContent;
       }
-
-        // if (!currentValue) {
-        //     display.textContent = '';
-        // }
-        // if (!operator) {
-        //     //firstNumber value is ALWAYS gunna be 9 never 99!
-        //     firstNumber += button.textContent; //9
-        //     updateDisplay(firstNumber);
-        // } else if (operator) {
-        //     secondNumber += button.textContent;
-        //     updateDisplay(secondNumber)
-        // }
     })
 })
 
@@ -104,36 +80,6 @@ operatorButtons.forEach((button)=> {
 equalsButton.addEventListener('click', () => operate(operator, firstNumber, secondNumber))
 
 
-
-
-
-
-// function operate() {
-//     console.log(firstNumber)
-//         console.log(secondNumber)
-//     if (operator && typeof firstNumber !== 'undefined' && typeof secondNumber !== 'undefined') {
-//       switch (operator) {
-        
-//         case '+':
-//           firstNumber += secondNumber;
-//           break;
-//         case '-':
-//           firstNumber -= secondNumber;
-//           break;
-//         case '*':
-//           firstNumber *= secondNumber;
-//           break;
-//         case '/':
-//           firstNumber /= secondNumber;
-//           break;
-//       }
-//       display.textContent = firstNumber;
-//       currentValue = '';
-//       operator = '';
-//       secondNumber = undefined;
-      
-//     }
-//   }
 
   function updateDisplay(result) {
     display.textContent = result;
